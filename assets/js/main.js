6f850131ad014ae9266934817d8998289547066a
Strategy102 <strategy954@gmail.com>
2022-06-20 06:54:27 -0700

    portfolio html css js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }                        
 // Menu
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
